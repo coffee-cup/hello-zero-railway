91caa20399d03971dde4c6e63ebc859158a92e4e
Jake Runzer <jakerunzer@gmail.com>
2025-05-15 23:42:10 -0700

    works in prod

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,20 +1,34 @@
 import Fastify, { type FastifyReply, type FastifyRequest } from "fastify";
+import fastifyStatic from "@fastify/static";
+import path from "path";
+
+const IS_PROD = process.env.NODE_ENV === "production";
 
 export const fastify = Fastify({
   logger: true,
 });
 
-fastify.get("/", function (request, reply) {
-  reply.send({ hello: "world" });
-});
+console.log("IS_PROD", IS_PROD);
+
+if (IS_PROD) {
+  const distPath = path.join(__dirname, "../../dist");
+  console.log(`Serving static files from ${distPath}`);
+  fastify.register(fastifyStatic, {
+    root: distPath,
+  });
+}
 
 fastify.get("/api/hello", function (request, reply) {
   reply.send({ hello: "world" });
 });
 
+fastify.get("/api/health", function (request, reply) {
+  reply.send({ status: "ok" });
+});
+
 const port = parseInt(process.env.PORT || "6789");
 
-fastify.listen({ port }, function (err, address) {
+fastify.listen({ port, host: "0.0.0.0" }, function (err, address) {
   if (err) {
     fastify.log.error(err);
     process.exit(1);
--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  root: "src/web",
   server: {
     port: 5678,
     proxy: {
